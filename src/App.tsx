d96ae0eda200a310c8241e332121e7b96e9ec53e
Ashwani <ashwanikaushik133@gmail.com>
2024-12-24 18:21:19 +0530

    initialize brainly BE

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Button } from './components/Button'
+
+function App() {
+  return <div>
+    <Button variant='primary' text='Add content'/>
+    <Button variant='secondary' text='Add content'/>
+  </div>
+}
+
+export default App
