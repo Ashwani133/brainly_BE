04482580e8daf4b59c26e4b5a5c187a8eb9acf9b
Ashwani <ashwanikaushik133@gmail.com>
2024-12-27 16:14:45 +0530

    check connect fe/be

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Dashboard } from "./pages/Dashboard";
 import { Signin } from "./pages/Signin";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Signup } from "./pages/Signup";
 function App(){
-  return <Signin />
-  
+
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/signup" element = {<Signup/>}/>
+    <Route path="/signin" element = {<Signin/>}/>
+    <Route path="/dashboard" element = {<Dashboard/>}/>
+  </Routes>
+  </BrowserRouter>
 }
 
 
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,7 @@
-export function Input({onChange, placeholder}:{placeholder:string; onChnage:()=>void}){
+interface InputProps{placeholder:string; reference?:any}
+
+export function Input({ placeholder, reference}:InputProps){
     return <div>
-        <input type="text" placeholder={placeholder} className="px-4 py-2 border rounded m-2" onChange={onChange}/>
+        <input ref={reference} type="text" placeholder={placeholder} className="px-4 py-2 border rounded m-2"/>
     </div>
 }
